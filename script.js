9aea283c404a587022809cc30aca8e43cb132bee
Dina Polousova <dinapolousova@gmail.com>
2023-03-23 09:50:49 +0300

    main files completed

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-let box = document.querySelector('.box'),
-    btnMore = document.querySelector('.more'),
-    btnLess = document.querySelector('.less'),
-    height = box.clientHeight,
-    isOpened = false;
+window.addEventListener('DOMContentLoaded', function() {
 
-initialSetup();
+    'use strict';
 
-btnMore.addEventListener('click', function() {
-    if(!isOpened) {
-        box.style.height = box.scrollHeight + 'px';
-        isOpened = true;
-        btnMore.classList.add('invisible');
-        btnLess.classList.remove('invisible');
-        console.log(isOpened);
-    } else if(isOpened) {
-        box.style.height = 240 + 'px';
-        btnMore.classList.remove('invisible');
-        btnLess.classList.add('invisible');
+    let box = document.querySelector('.box'),
+        btn = document.querySelector('button > span'),
+        button = document.querySelector('button'),
         isOpened = false;
-        console.log(isOpened);
-    }
-}); 
 
-function initialSetup() {
-    box.style.height = 240 + 'px';
-    box.scrollTop = 0;
-    btnLess.classList.add('invisible');
-    isOpened = false;
-}
+    initialSetup();
+
+    btn.addEventListener('click', function() {
+            if(!isOpened) {
+                box.style.height = box.scrollHeight + 'px';
+                console.log(box.scrollHeight);
+                button.style.top = 658 + 'px';
+                btn.textContent = 'show less';
+                btn.style.color = '#87cefa';
+                isOpened = true;  
+            } else if(isOpened) {
+                box.style.height = 238 + 'px';
+                btn.textContent = 'read more';
+                button.style.top = 290 + 'px';
+                btn.style.color = '#ff4400';
+                isOpened = false;
+            }
+    });
+
+
+    function initialSetup() {
+        box.style.height = box.clientHeight + 'px';
+        box.scrollTop = 0;
+    }
+});
